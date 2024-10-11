668b2313498218374224ae70cd963f8b05b49bde
AndrexCodes <123867051+AndrexCodes@users.noreply.github.com>
2024-10-11 10:15:08 +0300

    Update script wemasomo

--- wemasomo_script.js
+++ wemasomo_script.js
@@ -1,3 +1,11 @@
-const Test = ()=>{
-    alert("Hello")
+const SendResponse = (data)=>{
+    window.parent.postMessage(data, '*');
+}
+
+const ValidateResponse = (resp, ans, questionID)=>{
+    if(resp === ans){
+        send.lia("true")
+    }else{
+        send.lia("Incorrect please retry ↻", [], false)
+    }
 }
